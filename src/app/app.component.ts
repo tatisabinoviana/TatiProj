b1b019cf28a9883e30a743b1c3ddd3272fdd9b1a
Tatiana Viana <tatisabinoviana@gmail.com>
2022-12-27 21:58:56 -0300

    construindo o inicio do projeto, distribuindo as responsabilidades por quadros usando material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TatiProj';
+  titulo = 'Tatiana Viana';
+  habilidades = 'Habilidades';
+  pericias = 'Perícias';
+  levelUp = 'Ganhando Experiência';
+
+  isChecked = true;
+
+  // constructor(private _formBuilder: FormBuilder) {}
+
+  // color: ThemePalette = 'accent';
+  // checked = false;
+  // disabled = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    FormsModule,
+    MatGridListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
